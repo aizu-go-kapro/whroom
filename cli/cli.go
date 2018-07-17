30d308884d9faf49999797a4309e4498ff300992
Yuki Ito <acomagu@gmail.com>
2018-07-17 17:17:58 +0900

    fix nits

--- cli/cli.go
+++ cli/cli.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/mitchellh/cli"
-	"gopkg.in/zabawaba99/firego.v1"
 	"log"
 	"os"
 	"time"
+
+	"github.com/mitchellh/cli"
+	"gopkg.in/zabawaba99/firego.v1"
 )
 
-type Student struct{}
+type StudentCommand struct{}
 
-func (f *Student) Help() string {
-	return "whroom student"
+func (f *StudentCommand) Help() string {
+	return "Usage: whroom student <student_id>"
 }
 
-func (f *Student) Run(args []string) int {
+func (f *StudentCommand) Run(args []string) int {
 	if len(args) < 1 {
-		fmt.Printf("Please input studnet number\nex. whroom student s1240215")
-		return 0
+		log.Println("Please input studnet number\nex. whroom student s1240215")
+		return 1
 	}
 	k := args[0]
 	fg := firego.New("https://sao-unv.firebaseio.com", nil)
@@ -26,15 +27,22 @@ func (f *Student) Run(args []string) int {
 
 	var v map[string]interface{}
 	if err := fg.Child(k).Value(&v); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return 1
 	}
+	if v == nil {
+		log.Println("the student not record any location log yet.")
+		return 1
+	}
+
 	fmt.Printf("room: %s\n", v["room"])
 	fmt.Printf("timestamp: %s\n", v["timestamp"])
+
 	return 0
 }
 
-func (f *Student) Synopsis() string {
-	return "Print \"room and timestamp \""
+func (f *StudentCommand) Synopsis() string {
+	return "Print the room where the student is in."
 }
 
 func main() {
@@ -42,13 +50,14 @@ func main() {
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
 		"student": func() (cli.Command, error) {
-			return &Student{}, nil
+			return &StudentCommand{}, nil
 		},
 	}
 
 	exitStatus, err := c.Run()
 	if err != nil {
 		log.Println(err)
+		os.Exit(1)
 	}
 
 	os.Exit(exitStatus)
